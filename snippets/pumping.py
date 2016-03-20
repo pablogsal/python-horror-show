c9c5ec689f6a4950c6a92c6ae48d853b48ea55fe
Pablo Galindo <pablogsal@gmail.com>
2016-03-20 23:04:01 +0100

    Improved pump decorator and corutine

--- snippets/pumping.py
+++ snippets/pumping.py
@@ -1,31 +1,29 @@
 import itertools
 
 def pump(fun):
-    c = fun()
-    next(c)
-    def wrap( x ):
-        val =  c.send(x)
-        next(c)
-        return val
+    def wrap( *args, **kwargs ):
+        corutine =  fun(*args,**kwargs)
+        corutine.send(None)
+        return corutine.send
     return wrap
 
 @pump
 def ac():
     total = 0
     while True:
-        val = (yield)
-        total += val
         if total <= 0:
-            yield False
             total = 0
+            val = yield False
+            total += val
         else:
-            yield True
+            val = yield True
+            total += val
 
 
 
 lista = iter([1,2,3,-2,1,-5,1,2,3,4,5])
 
-a = itertools.groupby(lista,key = ac)
+a = itertools.groupby(lista,key = ac())
 
 b = [list(val) for test,val in a if test]
 
