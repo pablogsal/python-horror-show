8b5b7a89f021c5c3cb4d8210b6ad47c305305acb
Pablo Galindo <pablogsal@gmail.com>
2016-03-20 23:14:09 +0100

    Added docstrings to pumping.py

--- snippets/pumping.py
+++ snippets/pumping.py
@@ -1,30 +1,39 @@
+"""
+Test to use coroutines with itertools module to extend usage.
+"""
+
 import itertools
 
+
 def pump(fun):
-    def wrap( *args, **kwargs ):
-        corutine =  fun(*args,**kwargs)
+    """
+    Decorator to prime coroutines for use with itertools module
+    """
+    def wrap(*args, **kwargs):
+        corutine = fun(*args, **kwargs)
         corutine.send(None)
         return corutine.send
     return wrap
 
+
 @pump
-def ac():
+def acum_filter():
+    """
+    Coroutine filter that gives True if the current sum is positive and False if the sum reaches
+    zero or lower values. In the latter case, the current sum restarts to zero.
+    """
     total = 0
     while True:
         if total <= 0:
             total = 0
-            val = yield False
-            total += val
+            value = yield False
+            total += value
         else:
-            val = yield True
-            total += val
+            value = yield True
+            total += value
 
 
-
-lista = iter([1,2,3,-2,1,-5,1,2,3,4,5])
-
-a = itertools.groupby(lista,key = ac())
-
-b = [list(val) for test,val in a if test]
-
-print(b)
+test_values = iter([1, 2, 3, -2, 1, -5, 1, 2, 3, 4, 5])
+filtered_values = itertools.groupby(test_values, key=acum_filter())
+final_values = [list(val) for test, val in filtered_values if test]
+print(final_values)
